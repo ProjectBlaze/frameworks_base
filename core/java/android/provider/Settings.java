83e25ae13be183d1faf6f7a5ec996b0bf37bad36
maxwen <max.weninger@gmail.com>
2015-03-29 18:39:55 +0200

    base: allow disable of screenshot shutter sound [1/2]
    @ Stallix - Forward port to A13.
    
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5694,6 +5694,11 @@ public final class Settings {
         public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
 
         /**
+         * @hide
+         */
+        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
