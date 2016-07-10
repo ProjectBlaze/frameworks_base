3b481d529aec00d6283edcf57421677fb078d659
thecrazyskull <anaskarbila@aospa.co>
2016-07-10 06:32:58 -0400

    BatteryService: Add Dash charging support
    Change-Id: I5e79f057b80276401beaa55018eb240fb454be18

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2316,7 +2316,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         }
 
         // Take a guess at initial SIM state, battery status and PLMN until we get an update
+<<<<<<< HEAD
         mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, true, 0, 0, 0);
+=======
+        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, false, true);
+>>>>>>> bd4022d90a6c (BatteryService: Add Dash charging support)
 
         // Watch for interesting updates
         final IntentFilter filter = new IntentFilter();
@@ -3683,6 +3687,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
               (current.maxChargingWattage != old.maxChargingWattage ||
                current.maxChargingCurrent != old.maxChargingCurrent ||
                current.maxChargingVoltage != old.maxChargingVoltage)) {
+            return true;
+        }
+
+        // change in dash charging while plugged in
+        if (nowPluggedIn && current.dashChargeStatus != old.dashChargeStatus) {
             return true;
         }
 
