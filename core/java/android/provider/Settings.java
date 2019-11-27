472e3758d31beba342b4f377afbab7b11b8803ca
maxwen <max.weninger@gmail.com>
2019-11-27 16:27:40 +0100

    SystemUI: old style mobile data indicators [1/2]
    neobuddy89: Use Tuner API.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5662,6 +5662,11 @@ public final class Settings {
 
 
         /**
+         * @hide
+         */
+        public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
