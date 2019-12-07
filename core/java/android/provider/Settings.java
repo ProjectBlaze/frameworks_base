6af58b4845180bc1f76358b762a370ba0a9dd4a8
AnkitGourav <Ankitgourav035@gmail.com>
2019-12-07 15:43:55 +0530

    Add Network Traffic Layout [1/2]
    - Added option to select horizontal/verticle layout
    - horizontal layout was same as before
    - verticle layout is based on https://github.com/ArrowOS/android_frameworks_base/commit/e211cafe844abac4fb417400fe2e3caa0ff41202
    - Update units to KiB & MiB from KB & MB as of IEC standards
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
    
    Change-Id: I7ba19aaa7b3aeeed2ff833fb2fe6c86f9d83a82e
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5596,6 +5596,12 @@ public final class Settings {
          */
          public static final String NETWORK_TRAFFIC_FONT_SIZE = "network_traffic_font_size";
 
+         /**
+         * Network traffic layout
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_LAYOUT = "network_traffic_layout";
+
         /**
          * @hide
          */
@@ -5762,6 +5768,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_TYPE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_FONT_SIZE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_LAYOUT);
         }
 
         /**
