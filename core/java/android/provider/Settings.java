39e57493432e6fab8d36cd642b9ef28a63fe5390
DennySPb <dennyspb@gmail.com>
2020-10-05 12:52:54 +0300

    SystemUI: Optional haptic feedback on back gesture [1/2]
    Change-Id: I3bd949cde08ac9a0513e05f2356e84fde07ed767
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5701,6 +5701,9 @@ public final class Settings {
 	/** @hide */
         public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -5851,6 +5854,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VIBRATE_ON_DISCONNECT);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
+            PRIVATE_SETTINGS.add(BACK_GESTURE_HAPTIC);
         }
 
         /**
