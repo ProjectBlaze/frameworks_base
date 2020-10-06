9f60affbd729543a52c14b133e880c995971f4a0
htchoi1995 <htchoi1995@gmail.com>
2020-10-06 19:49:18 +0000

    Disable/Enable screenshot sound [1/2]
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5960,6 +5960,13 @@ public final class Settings {
         public static final String DATA_DISABLED_ICON = "data_disabled_icon";
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        @Readable
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
