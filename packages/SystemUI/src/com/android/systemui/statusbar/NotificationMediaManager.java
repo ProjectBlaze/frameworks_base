9d73c2699b1969dbead8e387ad126a747e9c92e8
Ido Ben-Hur <idoybh2@gmail.com>
2021-01-18 02:57:47 +0200

    NotificationMediaManager: Fixup lockscreen media art toggle
    Was broken after the fix for lockscreen wall here 31b44ed6abc6fae13e6f713a844a40fd2618c47f
    
    Change-Id: I2a2d25985500cc2fca88d1167a0604538001b322
    Signed-off-by:afterallafk <shivamatiet2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -587,12 +587,6 @@ public class NotificationMediaManager implements Dumpable {
      */
     public void updateMediaMetaData(boolean metaDataChanged, boolean allowEnterAnimation) {
         Trace.beginSection("StatusBar#updateMediaMetaData");
-        boolean mediaArt = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.KEYGAURD_MEDIA_ART, 0, UserHandle.USER_CURRENT) == 1;
-        if (!mediaArt) {
-            Trace.endSection();
-            return;
-        }
 
         if (mBackdrop == null) {
             Trace.endSection();
@@ -633,7 +627,7 @@ public class NotificationMediaManager implements Dumpable {
             }
             mProcessArtworkTasks.clear();
         }
-        if (artworkBitmap != null && mediaArt) {
+        if (artworkBitmap != null) {
             mProcessArtworkTasks.add(new ProcessArtworkTask(this, metaDataChanged,
                     allowEnterAnimation).execute(artworkBitmap));
         } else {
@@ -646,7 +640,11 @@ public class NotificationMediaManager implements Dumpable {
     private void finishUpdateMediaMetaData(boolean metaDataChanged, boolean allowEnterAnimation,
             @Nullable Bitmap bmp) {
         Drawable artworkDrawable = null;
-        if (bmp != null) {
+        // set media artwork as lockscreen wallpaper if player is playing
+        boolean mediaArt = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.KEYGAURD_MEDIA_ART, 0, UserHandle.USER_CURRENT) == 1;
+        if (bmp != null && (mediaArt || !ENABLE_LOCKSCREEN_WALLPAPER) &&
+                PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)) {
             artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), bmp);
         }
         boolean hasMediaArtwork = artworkDrawable != null;
