6e4d4e6d09c5c7a86a9db4294f6528268f8a2267
shivatejapeddi <shivatejapeddi@gmail.com>
2021-10-13 17:26:00 +0530

    base: PixelPropsUtils: Add Google Assistant to Enable 2.0 UI
    Change-Id: Ib78c08bfa3729c96aeeda8c77a8078ba11fbb475
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -57,6 +57,8 @@ public class PixelPropsUtils {
             "com.google.pixel.livewallpaper",
             "com.google.android.apps.safetyhub",
             "com.google.android.apps.turbo",
+            "com.google.android.apps.wallpaper",
+            "com.google.android.apps.googleassistant",
             "com.google.android.apps.maps",
             "com.google.android.gms",
             "com.google.android.gms",
