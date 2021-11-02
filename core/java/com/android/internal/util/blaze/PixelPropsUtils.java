9980b9f3180c2e8563ed7157cc0ef0503d65de57
Danny Lin <danny@kdrag0n.dev>
2021-11-01 20:06:48 -0700

    Limit SafetyNet workarounds to unstable GMS process
    The unstable process is where SafetyNet attestation actually runs, so
    we only need to spoof the model in that process. Leaving other processes
    fixes various issues caused by model detection and flag provisioning,
    including screen-off Voice Match in Google Assistant, broken At a Glance
    weather and settings on Android 12, and more.
    
    Change-Id: Idcf663907a6c3d0408dbd45b1ac53c9eb4200df8

--- core/java/com/android/internal/util/blaze/PixelPropsUtils.java
+++ core/java/com/android/internal/util/blaze/PixelPropsUtils.java
@@ -15,6 +15,7 @@
  */
 package com.android.internal.util.blaze;
 
+import android.app.Application;
 import android.os.Build;
 import android.os.SystemProperties;
 import android.util.Log;
@@ -172,11 +173,14 @@ public class PixelPropsUtils {
         propsToChangeMI11.put("MODEL", "M2102K1G");
     }
 
-    public static void setProps(String packageName) {
+    public static void setProps(Application app) {
+        final String packageName = app.getPackageName();
+        final String processName = app.getProcessName();
         if (packageName == null) {
             return;
         }
-        if (packageName.equals(PACKAGE_GMS)) {
+        if (packageName.equals(PACKAGE_GMS) &&
+                processName.equals(PACKAGE_GMS + ".unstable")) {
             sIsGms = true;
         }
         boolean isPixelDevice = Arrays.asList(pixelCodenames).contains(SystemProperties.get(DEVICE));
