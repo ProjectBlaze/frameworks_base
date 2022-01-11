fb89fccb6b426bea706d12ae059bc49956ded626
jhonboy121 <alfredmathew05@gmail.com>
2022-01-11 20:43:24 +0530

    SystemUI: allow disabling qs on secure lockscreen [1/2]
    * This does not prevent pulling down notification shade, qs will be entirely hidden even if you do and that's it.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -352,5 +352,6 @@ public class SecureSettingsValidators {
         VALIDATORS.put(Secure.GESTURE_NAVBAR_LENGTH_MODE, new InclusiveIntegerRangeValidator(0, 3));
         VALIDATORS.put(Secure.FACE_UNLOCK_METHOD, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.TETHERING_ALLOW_VPN_UPSTREAMS, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Secure.SECURE_LOCKSCREEN_QS_DISABLED, BOOLEAN_VALIDATOR);
     }
 }
