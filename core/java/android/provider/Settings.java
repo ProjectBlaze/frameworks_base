86f70fa21813456cd9d9ac55a85cd2e89c0943f0
Pranav Vashi <neobuddy89@gmail.com>
2022-01-17 21:20:04 +0530

    Allow toggling camera/mic privacy indicator [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10473,6 +10473,12 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Whether to show privacy indicator for camera
+         * @hide
+         */
+        public static final String ENABLE_CAMERA_PRIVACY_INDICATOR = "enable_camera_privacy_indicator";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
