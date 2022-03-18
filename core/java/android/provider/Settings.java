a7586be6bc40de64e85665039f95d8ee84ec5405
LibXZR <i@xzr.moe>
2022-03-18 10:12:37 +0800

    Ignore window secure flags [1/2]
    Feature: yt/KSCOPE-23
    Change-Id: Ia9447ca17f6b24152b64526c25710a77e1388ce6
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10861,6 +10861,13 @@ public final class Settings {
         public static final String SHOW_COMBINED_STATUS_BAR_SIGNAL_ICONS = "show_combined_status_bar_signal_icons";
 
         /**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        @Readable
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
