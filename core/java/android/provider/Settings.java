40e4fb0ae15d6f86ba32bb546cb917cb8f64a174
spkal01 <kalligeross@gmail.com>
2022-04-30 15:28:46 +0300

    base: QS Transparency level [1/2]
    Thanks to @Spark-Rom
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5604,6 +5604,11 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
         /**
+         * @hide
+         */
+        public static final String QS_TRANSPARENCY = "qs_transparency";
+
+        /**
          * These are all public system settings
          *
          * @hide
