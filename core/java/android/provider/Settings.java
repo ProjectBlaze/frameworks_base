7bbe2c72a58fb8966b2f83a34c0d968fe989f2d0
Ali B <abittin@gmail.com>
2022-05-27 11:10:22 +0000

    base: SystemUI: add keyguard weather style (1/2) Choose from Omni style with dedicated line of location, conditionimage, temperature or KeygardSlice style added to the dateline with conditionimage, temperature. Care is taken to properly display in ambient display, and ambient music.
    Change-Id: I6f09967843d360a82b6c98b8e3d1c9a269af798f
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5626,6 +5626,13 @@ public final class Settings {
         */
         public static final String OMNI_LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
 
+	/**
+        * 0: OmniJaws Style
+        * 1: KeyguardSlice Style
+        * @hide
+        */
+        public static final String AICP_LOCKSCREEN_WEATHER_STYLE = "lockscreen_weather_style";
+
         /**
          * Whether to display 4G icon instead LTE
          * @hide
@@ -6038,6 +6045,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(CAMERA_LAUNCH);
 	    PRIVATE_SETTINGS.add(OMNIJAWS_WEATHER_ICON_PACK);
 	    PRIVATE_SETTINGS.add(OMNI_LOCKSCREEN_WEATHER_ENABLED);
+	    PRIVATE_SETTINGS.add(AICP_LOCKSCREEN_WEATHER_STYLE);
         }
 
         /**
