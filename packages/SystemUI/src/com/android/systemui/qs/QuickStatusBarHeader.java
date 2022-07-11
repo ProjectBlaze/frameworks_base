c35ebd3d2ccd83fce7eeb2178a1753803ebcdc43
althafvly <althafvly@gmail.com>
2022-07-11 15:13:40 +0530

    SystemUI: Redo circle battery style
    - Now shows up on QuickSettings panel.
    - Fixes issues with battery circle style on theme and
      ui mode change and statusbar icon toggle.
    - Fixes wrong tint color with circle battery icon.
    - Re-evaluating system theme to avoid delay in circle
      battery tint color change.
    
    [KujouYuko | KaleidoscopeOS]: Adapt to AOSP Settings Provider.
    
    Change-Id: Id90f85e47ce4b9811e9bb67b91169997020300d8
    Signed-off-by: Kujou Yuko <i@779.moe>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -28,6 +28,8 @@ import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.provider.AlarmClock;
 import android.provider.CalendarContract;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Pair;
 import android.view.DisplayCutout;
@@ -320,7 +322,11 @@ public class QuickStatusBarHeader extends FrameLayout implements
 
         int textColor = Utils.getColorAttrDefaultColor(mContext, android.R.attr.textColorPrimary);
         if (textColor != mTextColorPrimary) {
+            boolean isCircleBattery = Settings.Secure.getIntForUser(
+                    mContext.getContentResolver(), Settings.Secure.STATUS_BAR_BATTERY_STYLE,
+                    0, UserHandle.USER_CURRENT) == 1;
             int textColorSecondary = Utils.getColorAttrDefaultColor(mContext,
+                    isCircleBattery ? android.R.attr.textColorHint :
                     android.R.attr.textColorSecondary);
             mTextColorPrimary = textColor;
             mClockView.setTextColor(textColor);
