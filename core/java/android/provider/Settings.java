55d33dfa672be6c6b867f40aafe264e87584803b
SamarV-121 <samarvispute121@pm.me>
2023-01-10 22:17:04 -0600

    base: Add settings to change QS tile shape [1/2]
    Change-Id: Ia8919fa755e5c865fc14cbeb9f4312e83cdbb750

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11664,6 +11664,12 @@ public final class Settings {
          */
         public static final String QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
+	/**
+         * Quicksettings tile shape
+         * @hide
+         */
+        public static final String QS_TILE_SHAPE = "qs_tile_shape";
+
         /**
          * Pulse navbar music visualizer
          * @hide
