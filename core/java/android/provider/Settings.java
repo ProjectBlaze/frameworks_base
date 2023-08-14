c35bc8d54b492d48bb83e9bc7f9444ff3a7f25e8
elpaablo <donopwhile1@gmail.com>
2023-08-14 01:04:57 +0100

    base: QS header images extended [1/2]
    * Add settings for:
        * tint
        * height
        * paddings
        * landscape (toggle)
        * transparency
        * custom hader image picker
    * Validate and limit height and tint opacity
    * Adjust ImageHelper to receive radius when rounding an image
      (used by Settings to display rounded header previews)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5851,10 +5851,71 @@ public final class Settings {
         public static final String TRANSPARENT_POWER_DIALOG_DIM = "transparent_power_dialog_dim";
 
         /**
-         * QS Header
+         * QS Header image
+         * format = "qs_header_image_" + QS_HEADER_IMAGE
+         * values 0 and -1 are reserved to mark disabled state (0) and custom user image (-1)
          * @hide
          */
         public static final String QS_HEADER_IMAGE = "qs_header_image";
+
+        /**
+         * QS header image tint
+         * 0 - No tint
+         * 1 - Accent
+         * 2 - Color primary
+         * 3 - Color primary inverse
+         * 4 - Custom
+         * @hide
+         */
+        public static final String QS_HEADER_IMAGE_TINT = "qs_header_image_tint";
+
+        /**
+         * QS header image custom tint
+         * @hide
+         **/
+        public static final String QS_HEADER_IMAGE_TINT_CUSTOM = "qs_header_image_tint_custom";
+
+        /**
+         * QS header image alpha
+         * @hide
+         **/
+        public static final String QS_HEADER_IMAGE_ALPHA = "qs_header_image_alpha";
+
+        /**
+         * QS header image portrait height
+         * @hide
+         **/
+        public static final String QS_HEADER_IMAGE_HEIGHT_PORTRAIT = "qs_header_image_height_portrait";
+
+        /**
+         * QS header image landscape height
+         * @hide
+         **/
+        public static final String QS_HEADER_IMAGE_HEIGHT_LANDSCAPE = "qs_header_image_height_landscape";
+
+        /**
+         * Wether to enable QS header image in landscape mode
+         * @hide
+         **/
+        public static final String QS_HEADER_IMAGE_LANDSCAPE_ENABLED = "qs_header_image_landscape_enabled";
+
+        /**
+         * QS header image side padding
+         * @hide
+         **/
+         public static final String QS_HEADER_IMAGE_PADDING_SIDE = "qs_header_image_padding_side";
+
+         /**
+          * QS header image top padding
+          * @hide
+          **/
+        public static final String QS_HEADER_IMAGE_PADDING_TOP = "qs_header_image_padding_top";
+
+        /**
+          * QS header image custom URI
+          * @hide
+          **/
+        public static final String QS_HEADER_IMAGE_URI = "qs_header_image_uri";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
@@ -6080,6 +6141,16 @@ public final class Settings {
             PRIVATE_SETTINGS.add(TRANSPARENT_POWER_MENU);
             PRIVATE_SETTINGS.add(TRANSPARENT_POWER_DIALOG_DIM);
             PRIVATE_SETTINGS.add(QS_HEADER_SIZE);
+            PRIVATE_SETTINGS.add(QS_HEADER_IMAGE);
+            PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_TINT);
+            PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_TINT_CUSTOM);
+            PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_ALPHA);
+            PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_HEIGHT_PORTRAIT);
+            PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_HEIGHT_LANDSCAPE);
+            PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_LANDSCAPE_ENABLED);
+            PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_PADDING_SIDE);
+            PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_PADDING_TOP);
+            PRIVATE_SETTINGS.add(QS_HEADER_IMAGE_URI);
         }
 
         /**
