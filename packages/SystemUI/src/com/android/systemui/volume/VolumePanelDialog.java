e1cb216dd207e197a057503eabf60dab094606ba
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-03 07:20:38 +0800

    SystemUI: Add per app volume to volume panel

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanelDialog.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanelDialog.java
@@ -221,6 +221,7 @@ public class VolumePanelDialog extends SystemUIDialog implements LifecycleOwner 
         uris.add(VOLUME_CALL_URI);
         uris.add(VOLUME_RINGER_URI);
         uris.add(VOLUME_ALARM_URI);
+        uris.add(APP_VOLUME_SLICE_URI);
         return uris;
     }
 
@@ -260,6 +261,12 @@ public class VolumePanelDialog extends SystemUIDialog implements LifecycleOwner 
             .authority(SETTINGS_SLICE_AUTHORITY)
             .appendPath(SettingsSlicesContract.PATH_SETTING_ACTION)
             .appendPath("alarm_volume")
+            .build();
+    private static final Uri APP_VOLUME_SLICE_URI = new Uri.Builder()
+            .scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(SETTINGS_SLICE_AUTHORITY)
+            .appendPath(SettingsSlicesContract.PATH_SETTING_ACTION)
+            .appendPath("app_volume")
             .build();
 
     private Uri getExtraControlUri() {
